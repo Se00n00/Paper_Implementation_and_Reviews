a5867818a331908ff031d5975592fd7ff8e6bfb1
Mohit kumar <Bluesky5april2021@gmail.com>
2025-05-04 21:03:35 +0530

    Implemented Point-Wise Feed Forward

--- Language/models/transformer.py
+++ Language/models/transformer.py
@@ -84,3 +84,19 @@ class MultiHeadAttention(nn.Module):
         # Final linear projection
         output = self.WO(attn_output)
         return output, attn_weights
+
+class FeedForward(nn.Module):
+    def __init__(self, model_dim, ff_dim, droupout=0.5):    # ff_dim is usally higher that model_dim
+        super(FeedForward, self).__init__()
+
+        self.fc1 = nn.Linear(model_dim, ff_dim)
+        self.fc2 = nn.Linear(ff_dim, model_dim)
+        self.relu = nn.ReLU()
+        self.droupout = nn.Dropout(droupout)
+    
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        x = self.droupout(x)       # apply dropout to the output of the second linear layer to reduce overfitting
+        return x
